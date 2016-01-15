1b897d1f90ebd37553c39de65619aa8b34c92bd7
Caglar Tanrikulu <ictanrik@yahoo.com>
2016-01-15 14:10:27 -0600

    just in case

--- test_stats.py
+++ test_stats.py
@@ -1,18 +1,19 @@
 #!/anaconda/bin/python
 from stats import *
+from nose.tools import assert_equal, assert_almost_equals
 
 def test_mean():
-        assert mean([2,4]) == 3.0
-test_mean()
+        assert_equal( mean([2,4]), 3.0)
+#test_mean()
 
 def test_empty_list():
         assert mean([]) == 0.0
-test_empty_list()
+#test_empty_list()
 
 
 def test_float_mean():
-	assert mean([0.2,0.5,1.1]) == 0.6
-test_float_mean()
+	assert_almost_equals( mean([0.5,0.5,1]), 0.666, 2 )
+#test_float_mean()
 
-exit()
+#exit()
 
