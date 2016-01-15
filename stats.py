19d95fa38c1022f11fac09ca9daa0e15780af235
Caglar Tanrikulu <ictanrik@yahoo.com>
2016-01-15 14:25:08 -0600

    best so far

--- stats.py
+++ stats.py
@@ -4,14 +4,18 @@ def mean(vals):
 	assert type(vals) is list, 'wrong input type'
 	if not vals:
 		return 0.0
-	return sum(vals)/len(vals)
+	nvals = len(vals)
+	vals = [float(x) for x in vals] #nice!
+	for i in range(len(vals)):
+		vals[i] = float(vals[i])
+	return sum(vals)/nvals
 
 
 
 if __name__ == '__main__':
 	print(mean( [2,4,5,7] ) )
 	#print(mean( "blah" ))
-
+	print( mean( ['1','2','3'] ) )
 
 exit()
 
